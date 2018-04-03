e9be03150f3dac64bdb34825445ca7941e05cff6
wutian0114 <wutian0114@163.com>
2018-04-03 18:59:37 +0800

    修改导航条

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,7 +4,13 @@
       <a class="header-logo" href="javascript:;"></a>
       <ul class="header-nav">
         <li><a href="#">首页</a></li>
-        <li><a href="#">解决方案</a></li>
+        <li>
+          <a href="javascript:;" @mouseover="showSolutionMore">解决方案</a>
+          <ul :class="isShowMore?'solution-more active':'solution-more'">
+            <li><a href="#">汽车销售系统</a></li>
+            <li><a href="#">签证云</a></li>
+          </ul>
+        </li>
         <li><a href="#">关于我们</a></li>
         <li><a href="#">联系方式</a></li>
         <li><a href="#">申请试用</a></li>
@@ -17,7 +23,8 @@
 export default {
   data(){
     return {
-      isScroll: false
+      isScroll: false,
+      isShowMore: false
     }
   },
   mounted(){
@@ -30,6 +37,13 @@ export default {
         this.isScroll = true;
       }else {
         this.isScroll = false;
+      }
+    },
+    showSolutionMore(){
+      if(this.isShowMore){
+        this.isShowMore = false;
+      }else {
+        this.isShowMore = true;
       }
     }
   }
@@ -64,18 +78,49 @@ export default {
 .nav-header .header-nav li {
   float: left;
 }
-.nav-header .header-nav li > a {
+.nav-header .header-nav > li > a {
   display: inline-block;
   line-height: 50px;
-  padding: 0 15px;
+  padding: 0 33px;
   text-decoration: none;
   color: #b1b1b1;
 }
-.nav-header .header-nav li:last-of-type > a {
+.nav-header .header-nav > li:last-of-type > a {
   background-color: #32d28c;
   color: #fff;
 }
-.nav-header .header-nav li > a:hover {
+.nav-header .header-nav > li:nth-of-type(2) {
+  position: relative;
+}
+.nav-header .header-nav > li > a:hover {
   color: #fff;
+}
+.nav-header .header-nav .solution-more {
+  list-style: none;
+  padding: 0;
+  position: absolute;
+  top: 49px;
+  right: 0;
+  width: 130px;
+  height: 0;
+  overflow: hidden;
+  background-color: #ffffff;
+  transition: height .5s;
+}
+.nav-header .header-nav .solution-more.active {
+  height: 100px;
+  border: 1px solid rgba(0, 0, 0, .7);
+}
+.nav-header .header-nav .solution-more > li > a {
+  display: inline-block;
+  line-height: 50px;
+  text-decoration: none;
+  color: #1d253c;
+  width: 130px;
+  text-align: center;
+}
+.nav-header .header-nav .solution-more > li > a:hover {
+  background-color: #32d28c;
+  color: #ffffff;
 }
 </style>
